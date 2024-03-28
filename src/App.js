a10e23572a2ef6ab03188cdda7652d9256b5419f
ayush844 <ayush.xyz1625@gmail.com>
2024-03-28 14:44:01 +0530

    left sidebar
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import './App.css';
-import Header from './Header/Header';
+import Header from './Components/Header/Header';
+import LeftSidebar from './Components/Sidebar/LeftSidebar';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className="app_body">
+        <LeftSidebar />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Sidebar/LeftSidebar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './LeftSidebar.css'
+import { Avatar } from '@mui/material'
+
+const LeftSidebar = () => {
+
+    const recentItems = (topic)=>{
+        return(
+            <div className="sidebar_recentItems">
+                <span className="sidebar_hash">#</span>
+                <p>{topic}</p>
+            </div>
+        )
+
+    }
+
+  return (
+    <div className="leftSidebar">
+        <div className="sidebar_top">
+            <img src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="cover" />
+            <Avatar className='sidebar_avatar'/>
+            <h2>Ayush Sharma</h2>
+            <h4>Java programmer || DSA || full stack developer || let's connect</h4>
+        </div>
+        <div className="sidebar_stats">
+            <div className="sidebar_stat"><p>Profile viewers: </p> <p className='sidebar_stat_number'>36</p></div>
+            <div className="sidebar_stat"><p>Connections: </p><p className='sidebar_stat_number'>527</p></div>
+        </div>
+        <div className="sidebar_bottom">
+            <p>Recent</p>
+            <div className="divider"></div>
+            {recentItems('Reactjs')}
+            {recentItems('Blockchain')}
+            {recentItems('Software')}
+            {recentItems('google')}
+            {recentItems('Recession')}
+        </div>
+    </div>
+  )
+}
+
+export default LeftSidebar
