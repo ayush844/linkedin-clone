a425e4d7fc9b05f341100e04505503009bc2f72f
ayush844 <ayush.xyz1625@gmail.com>
2024-03-30 18:02:28 +0530

    other's post
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Feed.css'
 import Post from './Post'
+import OthersPost from './OthersPost'
 
 const Feed = () => {
   return (
     <div className='feed-section'>
         <Post />
         <div className="divider"></div>
+        <OthersPost />
+        <OthersPost />
+        <OthersPost />
+        <OthersPost />
+        <OthersPost />
+        <OthersPost />
     </div>
   )
 }
---
+++ src/Components/Feed/OthersPost.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './OthersPost.css'
+
+const OthersPost = () => {
+  return (
+    <div className='othersPost'>
+        <div className="authorInfo">
+            <div className="author-avatar-div">
+                <img src="https://images.pexels.com/photos/2128807/pexels-photo-2128807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="avatar" />
+            </div>
+            <div className="authorNameAndProfession">
+                <h3>Vaibhav Jha</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, labore. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia facilis porro repudiandae eligendi odio deleniti! Magni quaerat neque architecto explicabo atque sint aliquam accusantium, eligendi voluptatum maxime quos, saepe iure.</p>
+            </div>
+            
+        </div>
+        <div className="postContent">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio tempora quisquam autem excepturi soluta ab voluptatibus libero fuga tenetur, corporis velit incidunt officia veritatis eligendi quia exercitationem necessitatibus commodi sequi nostrum repellendus, natus ea id. Excepturi, natus deleniti autem consectetur officiis magni ipsam? Vitae enim dolorum laboriosam tempora explicabo? A fuga architecto debitis minima harum consequuntur voluptatem omnis amet, iure quasi facilis aut fugit neque nesciunt sequi, aperiam minus quisquam, consectetur cumque necessitatibus voluptas adipisci. Dolorem, a temporibus porro explicabo officiis omnis! Officiis fugiat sequi odit ea, qui quod minima.</p>
+        </div>
+    </div>
+  )
+}
+
+export default OthersPost
