0e6b2eeaa33220b4eaade1cab65d9e0de8a1c401
ayush844 <ayush.xyz1625@gmail.com>
2024-06-25 14:49:46 +0530

    made the my connection page responsive
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- src/Components/Cards/MyConnectionRectangle.jsx
+++ src/Components/Cards/MyConnectionRectangle.jsx
@@ -22,7 +22,9 @@ const MyConnectionRectangle = () => {
                 <img src="https://images.unsplash.com/photo-1610130383669-95917c70ca20?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="connection avatar" />
             </div>
             <div className="nameAndBio">
-                <h3>Ashima Mishra</h3>
+                <div className="nameAndBioName">
+                    <h3>Ashima Mishra</h3>
+                </div>
                 <div className="nameAndBioBio">
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere accusamus tempore mollitia sunt velit maxime nesciunt nam asperiores quia? Fugit veniam quam est totam deserunt eligendi. Incidunt accusantium id rerum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque obcaecati inventore tenetur eos reprehenderit distinctio natus molestiae totam fuga, asperiores, aliquid ea. Eius, labore animi? Soluta, rem suscipit! Repellendus, vero?</p>
                 </div>
