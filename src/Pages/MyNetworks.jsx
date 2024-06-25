d7e866fe0930cb377af8ef85c8b0e0576494be3f
ayush844 <ayush.xyz1625@gmail.com>
2024-06-25 07:29:27 +0530

    making my networks page responsive
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- src/Pages/MyNetworks.jsx
+++ src/Pages/MyNetworks.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import NetworksLeftSidebar from '../Components/Sidebar/NetworksLeftSidebar'
 import NetworkRightSidebar from '../Components/Sidebar/NetworkRightSidebar'
 
+import './MyNetworks.css'
+
 const MyNetworks = () => {
   return (
-    <>
+    <main>
     <NetworksLeftSidebar />
     <NetworkRightSidebar />
 
-    </>
+    </main>
   )
 }
 
