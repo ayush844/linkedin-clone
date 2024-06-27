961148606ad42588f0c6e50e8dde27abb1036f58
ayush844 <ayush.xyz1625@gmail.com>
2024-06-27 11:51:07 +0530

    make th notification page responsive
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- src/Components/Cards/NotificationRectagle.jsx
+++ src/Components/Cards/NotificationRectagle.jsx
@@ -1,21 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './NotificationRectangle.css'
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-
-import DeleteIcon from '@mui/icons-material/Delete';
 
 const NotificationRectagle = () => {
 
-
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
-
-    const handleRemove = () => {
-        setIsOpen(false);         
-    };
 
   return (
     <div className='notificationRectangle'>
@@ -28,19 +15,6 @@ const NotificationRectagle = () => {
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus sint nihil ut saepe, totam earum sapiente officiis laboriosam commodi corporis sed voluptatem quidem atque in quaerat ab inventore perferendis cum aliquam consequatur error. Sed alias facere repellat, quasi voluptatibus nam quam autem tenetur, hic modi qui cum quas, quidem sit.</p>
                 </div>
             </div>
-        </div>
-        <div className="buttonOptions">
-            <div className="horizontalDots" onClick={toggleMenu}>
-                <MoreHorizIcon />
-            </div>
-            {isOpen && (
-                <div className="menu-options">
-                     <div className="option" onClick={handleRemove}>
-                        <DeleteIcon />
-                        <span>Remove Notification</span>
-                    </div>          
-                </div>
-            )}
         </div>
 
     </div>
